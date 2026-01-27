7fcbc4e251e56efec340b9d12e9e084cf184ba08
jonathanibarra <maosied@gmail.com>
2026-01-27 23:33:46 +0100

    Initial Commit

---
+++ Recetas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recetas
+//
+//  Created by Jonathan Ibarra on 27/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
